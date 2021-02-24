ada30f914e83184cd927abbbf0314cf591e0a252
smfilho <smfilho@gmail.com>
2021-02-24 12:19:41 -0600

    half countdown done

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,32 @@
+import { ExperienceBar } from '../components/ExperienceBar'
+import { Profile } from '../components/Profile'
+import { CompletedChallenges } from '../components/CompletedChallenges'
+import { Countdown } from '../components/Countdown'
+
 import Head from 'next/head'
 
-import { ExperienceBar } from "../components/ExperienceBar/ExperienceBar";
+import styles from '../styles/pages/Home.module.css'
 
 export default function Home() {
   return (
-    <div className="container">
+    <div className={styles.container}>
+      <Head>
+        <title>Start | HavABreaK </title>
+      </Head>
+      
       <ExperienceBar />
+
+      <section>
+        <div>
+          <Profile />
+          <CompletedChallenges />
+          <Countdown />
+        </div>
+        <div> 
+          
+        </div>
+      </section>
+
     </div>
   )
 }
