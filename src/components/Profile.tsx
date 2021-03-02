b26a10fcb236c128cb4d69f2e71eeba227870992
Silvio Melo Filho <smfilho@gmail.com>
2021-03-01 22:57:55 -0600

    Done: Almost all functionallities

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,18 +1,16 @@
-import styles from '../styles/components/Profile.module.css'
+import styles from "../styles/components/Profile.module.css";
 
 export function Profile() {
-   return (
-     <div className={styles.profileContainer}>
-       <img src="https://github.com/smfilho.png" alt="" className="src"/>
-       <div>
-         <strong>Silvio Melo Filho</strong>
-         <p>
+  return (
+    <div className={styles.profileContainer}>
+      <img src="https://github.com/smfilho.png" alt="" className="src" />
+      <div>
+        <strong>Silvio Melo Filho</strong>
+        <p>
           <img src="icons/level.svg" alt="Level" />
           Level 1
-         </p>
-       </div>
-     </div>
-   )
- }
-
- 
+        </p>
+      </div>
+    </div>
+  );
+}
