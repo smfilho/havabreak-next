9b2ff9e384b087fab5ff7210d9c3677e12c13e95
Silvio Melo Filho <smfilho@gmail.com>
2021-03-03 11:36:51 -0600

    Added: Cookies feature

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,35 +1,61 @@
-import { ExperienceBar } from '../components/ExperienceBar'
-import { Profile } from '../components/Profile'
-import { CompletedChallenges } from '../components/CompletedChallenges'
-import { Countdown } from '../components/Countdown'
-import { ChallengeBox } from '../components/ChallengeBox'
+import Head from "next/head";
+import { GetServerSideProps } from "next";
 
-import Head from 'next/head'
+import { ExperienceBar } from "../components/ExperienceBar";
+import { Profile } from "../components/Profile";
+import { CompletedChallenges } from "../components/CompletedChallenges";
+import { Countdown } from "../components/Countdown";
+import { ChallengeBox } from "../components/ChallengeBox";
+import { CountdownProvider } from "../contexts/CountdownContext";
 
-import styles from '../styles/pages/Home.module.css'
-import { CountdownProvider } from '../contexts/CountdownContext'
+import styles from "../styles/pages/Home.module.css";
+import { ChallengesProvider } from "../contexts/ChallengesContext";
 
-export default function Home() {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>Start | HavABreaK </title>
-      </Head>
-      
-      <ExperienceBar />
-
-      <CountdownProvider>
-        <section>
-          <div>
-            <Profile />
-            <CompletedChallenges />
-            <Countdown />
-          </div>
-          <div> 
-            <ChallengeBox />
-          </div>
-        </section>
-      </CountdownProvider>
-    </div>
-  )
+interface HomeProps {
+  level: number;
+  currentExperience: number;
+  challengesCompleted: number;
 }
+
+export default function Home(props: HomeProps) {
+  return (
+    <ChallengesProvider
+      level={props.level}
+      currentExperience={props.currentExperience}
+      challengesCompleted={props.challengesCompleted}
+    >
+      <div className={styles.container}>
+        <Head>
+          <title>Start | HavABreaK </title>
+        </Head>
+
+        <ExperienceBar />
+
+        <CountdownProvider>
+          <section>
+            <div>
+              <Profile />
+              <CompletedChallenges />
+              <Countdown />
+            </div>
+            <div>
+              <ChallengeBox />
+            </div>
+          </section>
+        </CountdownProvider>
+      </div>
+    </ChallengesProvider>
+  );
+}
+
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  const { level, currentExperience, challengesCompleted } = ctx.req.cookies;
+
+  return {
+    props: {
+      level: Number(level),
+      currentExperience: Number(currentExperience),
+      challengesCompleted: Number(challengesCompleted),
+    },
+  };
+};
